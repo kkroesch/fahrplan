e9cd4dbe4927dac7259ca585ce0800a7f7166d29
Karsten Kroesch <kkroesch@gmail.com>
2016-11-05 21:10:08 +0100

    No Python 3 compatible.

--- util.py
+++ util.py
@@ -5,16 +5,16 @@ class Map(dict):
         Example:
             m = Map({'first_name': 'Eduardo'}, last_name='Pool', age=24, sports=['Soccer'])
     """
-    
+
     def __init__(self, *args, **kwargs):
         super(Map, self).__init__(*args, **kwargs)
         for arg in args:
             if isinstance(arg, dict):
-                for k, v in arg.iteritems():
+                for k, v in arg.items():
                     self[k] = v
 
         if kwargs:
-            for k, v in kwargs.iteritems():
+            for k, v in kwargs.items():
                 self[k] = v
 
     def __getattr__(self, attr):
