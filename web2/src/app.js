d79e3146901fa606c0ad13cbd686a2d4e9e35981
Karsten Kroesch <kkroesch@gmail.com>
2016-11-09 14:03:12 +0100

    Added API call and button handler.

--- web2/src/app.js
+++ web2/src/app.js
@@ -1,6 +1,11 @@
 import {Bookmark} from './bookmark';
 import $ from 'jquery';
 
+function reformatTime(datetime) {
+  	var d = new Date(datetime)
+  	return `${d.getHours()}:${d.getMinutes()}`;
+  }
+
 export class App {
   constructor() {
     this.heading = "Bookmarks";
@@ -25,9 +30,19 @@ export class App {
 
   }
 
-  nextConnection() {
-  	$.get('package.json', data => {
-  		console.log(data);
+  nextConnection(bookmark) {
+  	bookmark.journey = '<ol>';
+  	$.get('http://transport.opendata.ch/v1/connections?from=008503000&to=008502110&limit=1', fahrplan => {
+  		for (var connection of fahrplan.connections) {
+  			for (var section of connection.sections) {
+  				if (section.journey == null) continue;
+  				bookmark.journey += `<li>Mit ${section.journey.name} ` +
+  					`von ${section.departure.station.name} ` + 
+  					`um ${reformatTime(section.departure.departure)} ` +
+  					`nach ${section.journey.to} (${section.arrival.station.name} an: ${reformatTime(section.arrival.arrival)})</li>`;
+  			}
+  		}
+  		bookmark.journey += '</ol>';
   	})
   }
 }
--- web2/src/bookmark.js
+++ web2/src/bookmark.js
@@ -3,5 +3,6 @@ export class Bookmark {
   constructor(start, destination) {
     this.start = start;
     this.destination = destination;
+    this.journey = '';
   }
 }
