07f2bc154581c188cb776ec149fad1fdfeb6c3b2
Kroesch Karsten, INI-ONE-SCC-VMI <Karsten.Kroesch@swisscom.com>
2019-12-10 16:25:39 +0100

    Added icons. And Python 3 :-).

--- sbb.py
+++ sbb.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # -*- coding: utf-8 -*-
 
@@ -11,7 +11,21 @@ import datetime
 import dateutil.parser
 import requests
 
-from util import Colors
+class Colors:
+    """ Colors for console putput as ANSI code.
+        Example usage:
+            exit(Colors.ERROR.format('Error: API token not given.'))
+            print(Colors.OKGREEN.format("All went fine.")
+    """
+    HEADER = '\033[95m{}\033[0m'
+    OKBLUE = '\033[94m{}\033[0m'
+    OKGREEN = '\033[92m{}\033[0m'
+    ERROR = '\033[91m{}\033[0m'
+    WARNING = '\033[93m{}\033[0m'
+    FAIL = '\033[91m{}\033[0m'
+    BOLD = '\033[1m{}\033[0m'
+    UNDERLINE = '\033[4m{}\033[0m'
+    ENDC = '\033[0m'
 
 
 def reformat_time(timestring):
--- stations.py
+++ stations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # -*- coding: utf-8 -*-
 
@@ -9,6 +9,13 @@ import json
 import argparse
 import requests
 
+icons = {
+	"train": "",
+	"bus": "ﲞ",
+	"tram": "館",
+	None: "",
+}
+
 parser = argparse.ArgumentParser()
 parser.add_argument('station', type=str)
 parser.add_argument('-v', '--verbose', help="Increase output verbosity.", action="store_true")
@@ -18,4 +25,4 @@ req = requests.get('http://transport.opendata.ch/v1/locations?query=' + argument
 station_list = req.json()
 
 for station in station_list['stations']:
-	print(station.get('id', '-'*9), station['name'])
+	print(f"{icons[station['icon']]} {station['id']}  {station['name']}")
